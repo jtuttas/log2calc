7a48c7154e6cdf7fe8091c19d9689cd8c5523625
jtuttas <jtuttas@gmx.net>
2024-10-17 16:26:00 +0200

    V1.1

--- calculator.js
+++ calculator.js
@@ -22,8 +22,8 @@ function createCalculator(containerId) {
             <button onclick="appendNumber('0', event)" class="calculator-button">0</button>
             <button onclick="appendDecimal(event)" class="calculator-button">.</button>
             <button onclick="toggleSign(event)" class="calculator-button">+/-</button>
-            <button onclick="setOperation('divide', event)" class="calculator-button">/</button>
             <button onclick="setOperation('log2', event)" class="calculator-button">log2</button>
+            <button onclick="setOperation('divide', event)" class="calculator-button">/</button>
             <button id="clear" onclick="clearDisplay(event)" class="calculator-button">C</button>
             <button id="equals" onclick="calculate(event)" class="calculator-button" style="grid-column: span 2;">=</button>
         </div>
@@ -31,7 +31,7 @@ function createCalculator(containerId) {
 }
 
 let currentInput = "";
-document.getElementById("display").value = "0";
+
 let operation = "";
 let firstNumber = null;
 let result = null;
@@ -76,7 +76,7 @@ function setOperation(op, event) {
   } else {
     currentInput = "";
     operation = op;
-    document.getElementById("display").value = "0";
+    document.getElementById("display").value = "";
   }
 }
 
